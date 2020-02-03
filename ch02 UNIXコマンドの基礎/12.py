ac4bd30c530ef68dbca7650af51987be493cd1ce
ambiguoustexture <svuvyrfvync@gmail.com>
2020-02-04 01:11:25 +0800

    Add 12.py

---
+++ ch02 UNIXコマンドの基礎/12.py
@@ -0,0 +1,13 @@
+# Author：ambiguoustexture
+# Date: 2020-02-03
+
+file = 'hightemp.txt'
+
+with open(file) as text:
+    col1 = open('col1.txt', 'w')
+    col2 = open('col2.txt', 'w')
+
+    for line in text:
+        items = line.split('\t')
+        col1.write(items[0] + '\n')
+        col2.write(items[1] + '\n')
