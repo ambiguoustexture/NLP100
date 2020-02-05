d5bc61916522664f26984a3c5f74a62f59334ee3
ambiguoustexture <svuvyrfvync@gmail.com>
2020-02-06 00:47:44 +0800

    Add 22.py

---
+++ ch03 第3章 正規表現/22.py
@@ -0,0 +1,24 @@
+# Author：ambiguoustexture
+# Date: 2020-02-06
+import re
+
+pattern = re.compile(r'''
+	^		# Head of line
+	.*		# Zero or more arbitrary characters
+	\[\[Category:
+	(.*?)	        # Capture target, 0 or more arbitrary characters, non-greedy match 
+	(?:\]\]|\|)	# Not captured, ']]' or '|'
+	.*		# Zero or more arbitrary characters
+	$		# End of line
+	''', re.MULTILINE + re.VERBOSE)
+
+file = 'UK.txt'
+with open(file) as text:
+    text = text.read()
+    res = pattern.findall(text)
+    for line in res:
+        print(line)
+
+
+
+
