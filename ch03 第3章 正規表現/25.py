523a621b9351044880c60d99aad9b64a154aa97e
ambiguoustexture <svuvyrfvync@gmail.com>
2020-02-07 02:02:54 +0800

    Add stuff.

---
+++ ch03 第3章 正規表現/25.py
@@ -0,0 +1,42 @@
+# Author：ambiguoustexture
+# Date: 2020-02-07
+import re
+
+pattern_contents = re.compile(r'''
+        ^\{\{基礎情報.*?$   # Line starting with '{{基礎情報'
+        (.*?)               # Capture target,
+                            # 0 or more arbitrary characters,
+                            # non-greedy match
+        ^\}\}$              # Line end with '}}'
+        ''', re.MULTILINE + re.VERBOSE + re.DOTALL)
+
+pattern_fields = re.compile(r'''
+        ^\| 
+        (.+?) 
+        \s* 
+        =
+        \s* 
+        (.+?)
+        (?:         # Start ungrouped group 
+        (?=\n\|)    # Before line feed + '|' (positive look-ahead) 
+        | (?=\n$)   # Or line feed + before end (positive look-ahead) 
+        )           # End of group
+        ''', re.MULTILINE + re.VERBOSE + re.DOTALL)
+
+file = 'UK.txt'
+
+with open(file) as text:
+    text = text.read()
+    contents = pattern_contents.findall(text)
+    fields = pattern_fields.findall(contents[0])
+
+    res = {}
+    keys = []
+    for field in fields:
+        res[field[0]] = field[1]
+        keys.append(field[0])
+    
+    # use keys for confirmation
+    for item in sorted(res.items(), 
+            key = lambda field: keys.index(field[0])):
+        print(item)
