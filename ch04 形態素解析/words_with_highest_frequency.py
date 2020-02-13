aa1b038e01a93f14c08388918d436a47d75fa29c
ambiguoustexture <svuvyrfvync@gmail.com>
2020-02-13 22:59:53 +0800

    Update words_with_highest_frequency.py to remove punctuations.

--- ch04 形態素解析/words_with_highest_frequency.py
+++ ch04 形態素解析/words_with_highest_frequency.py
@@ -6,9 +6,13 @@ from morphological_analysis import morphology_map
 
 file_parsed = "./neko.txt.mecab"
 words = morphology_map(file_parsed)
+words_without_punctuation = []
+for word in words:
+    if word['pos'] != '記号':
+        words_without_punctuation.append(word)
 
 word_count = Counter()
-word_count.update([word['surface'] for word in words])
+word_count.update([word['surface'] for word in words_without_punctuation])
 
 items = word_count.most_common()
 for item in items:
