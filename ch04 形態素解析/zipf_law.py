e4ee6fd73925b2abe2ed3e297870144957e26d4d
ambiguoustexture <svuvyrfvync@gmail.com>
2020-02-14 00:44:25 +0800

    Add zipf_law.py

---
+++ ch04 形態素解析/zipf_law.py
@@ -0,0 +1,32 @@
+# Author：ambiguoustexture
+# Date: 2020-02-13
+
+import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
+from collections import Counter
+from morphological_analysis import morphology_map
+
+file_parsed = "./neko.txt.mecab"
+words = morphology_map(file_parsed)
+words_without_punctuation = []
+for word in words:
+    if word['pos'] != '記号':
+        words_without_punctuation.append(word)
+
+word_count = Counter()
+word_count.update([word['surface'] for word in words_without_punctuation])
+
+items  = word_count.most_common()
+items  = list(zip(*items))
+counts = items[1]
+rank = list(range(1, len(counts) + 1))
+
+fig = plt.figure()
+# fp = FontProperties(fname=r'./MS-Mincho.ttf', size=14)
+plt.plot(counts, rank)
+plt.xlabel('rank')
+plt.ylabel('Frequency')
+plt.xscale('log')
+plt.yscale('log')
+plt.title('Zipf Law')
+plt.show()
