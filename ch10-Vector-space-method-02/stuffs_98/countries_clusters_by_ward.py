314309b841baca4f82e31b8f8ca86b0cd2397c07
ambiguoustexture <svuvyrfvync@gmail.com>
2020-03-12 21:19:28 +0800

    Update README.

--- ch10-Vector-space-method-02/stuffs_98/countries_clusters_by_ward.py
+++ ch10-Vector-space-method-02/stuffs_98/countries_clusters_by_ward.py
@@ -2,15 +2,13 @@
 # Date: 2020-03-11
 
 import pickle
-from collections import OrderedDict
 from scipy import io
-import numpy as np
 
 from scipy.cluster.hierarchy import ward, dendrogram
 from matplotlib import pyplot as plt
 
-file_t_index_dict = './stuffs_96/t_index_dict_countries'
-file_matrix       = './stuffs_96/matrix_countries'
+file_t_index_dict = '../stuffs_96/t_index_dict_countries'
+file_matrix       = '../stuffs_96/matrix_countries'
 
 with open(file_t_index_dict, 'rb') as t_index_dict:
     t_index_dict = pickle.load(t_index_dict)
